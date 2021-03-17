7b463fadb2123085f0363853c941a79d3325880e
Vagner Wentz <wentz.vagner@gmail.com>
2021-03-16 23:14:44 -0300

    feat: create the TransactionsTable component.

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,39 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento do website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Software</td>
+            <td>12/03/2021</td>
+          </tr>
+          <tr>
+            <td>Freelance para a Uber</td>
+            <td className="deposit">R$20.000</td>
+            <td>Software</td>
+            <td>05/02/2021</td>
+          </tr>
+          <tr>
+            <td>Compra do Macbook Pro</td>
+            <td className="withdraw">-R$20.000</td>
+            <td>Compras</td>
+            <td>05/03/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
