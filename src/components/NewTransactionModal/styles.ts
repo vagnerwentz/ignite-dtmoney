b5d7437ba76ba46619e52c486150a9f07c3ef404
Vagner Wentz <wentz.vagner@gmail.com>
2021-03-17 22:53:01 -0300

    style: add the TransactionTypeContainer style and the buttons styles.

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.form`
   h2 {
@@ -12,7 +13,7 @@ export const Container = styled.form`
     padding: 0 1.5rem;
     height: 4rem;
     border-radius: 0.25rem;
-    border: 1px solid var(--input-border);
+    border: 1px solid var(--border);
     background: var(--input-background);
     font-weight: 400;
     font-size: 1rem;
@@ -42,6 +43,43 @@ export const Container = styled.form`
 
     &:hover {
       filter: brightness(0.9);
+    }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.5rem;
+
+  button {
+    height: 4rem;
+    border: 1px solid var(--border);
+    border-radius: 0.25rem;
+
+    background: transparent;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s;
+
+    &:hover {
+      border-color: ${darken(0.1, 'var(--border)')};
+    }
+
+    img {
+      width: 20px;
+      height: 20px;
+    }
+
+    span {
+      display: inline-block;
+      margin-left: 1rem;
+      font-size: 1rem;
+      color: var(--text-title);
     }
   }
 `;
