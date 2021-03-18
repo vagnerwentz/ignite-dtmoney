672cc1b351a20252884bc7c8599c6a916da54dc0
Vagner Wentz <wentz.vagner@gmail.com>
2021-03-17 21:14:49 -0300

    feat: add miragejs with the transaction data.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import App from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Macbook Pro',
+          amount: 20000,
+          type: 'withdraw',
+          category: 'Computer',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
