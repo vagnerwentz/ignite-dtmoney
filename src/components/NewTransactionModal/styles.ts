14d180aae8f54df98ce8e429d4f71094adc1ac4a
Vagner Wentz <wentz.vagner@gmail.com>
2021-03-17 23:05:40 -0300

    style: create the RadioBox component to style the button with property.

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import { darken } from 'polished';
 
+interface RadioBoxProps {
+  isActive: boolean;
+}
+
 export const Container = styled.form`
   h2 {
     color: var(--text-title);
@@ -52,34 +56,34 @@ export const TransactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid var(--border);
-    border-radius: 0.25rem;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid var(--border);
+  border-radius: 0.25rem;
 
-    background: transparent;
+  background: ${(props) => props.isActive ? '#eee' : 'transparent'};
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    transition: border-color 0.2s;
+  transition: border-color 0.2s;
 
-    &:hover {
-      border-color: ${darken(0.1, 'var(--border)')};
-    }
+  &:hover {
+    border-color: ${darken(0.1, '#D7D7D7')};
+  }
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  img {
+    width: 20px;
+    height: 20px;
+  }
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
